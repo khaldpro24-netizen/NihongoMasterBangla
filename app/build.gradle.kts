7d768d6e2c35e80aac09c60dd9f0d22323a77e7e
khaldpro24-netizen <khaldpro24@gmail.com>
2026-09-24 13:10:12 +0900

    Release 1.2 with learning feature update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id(
 android {
  namespace="com.nihongomaster.bangla"
  compileSdk=35
- defaultConfig { applicationId="com.nihongomaster.bangla"; minSdk=26; targetSdk=35; versionCode=2; versionName="1.1" }
+ defaultConfig { applicationId="com.nihongomaster.bangla"; minSdk=26; targetSdk=35; versionCode=3; versionName="1.2" }
  compileOptions {
    sourceCompatibility = JavaVersion.VERSION_17
    targetCompatibility = JavaVersion.VERSION_17
